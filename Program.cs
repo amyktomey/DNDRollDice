6aa798bdbc0d80c5fb363d4cd5c1dc2c268c331b
Amy Tomey <amy.tomey59@@gmail.com>
2022-03-08 17:12:32 -0500

    initial code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//using CodeLouisvilleLibrary;
+using DNDRollDice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       //insert menu - list of available dice to roll
+
+        RollDie sixSided = new RollDie();
+        RollDie tenSided = new RollDie(10);
+
+        Console.WriteLine(sixSided);
+
+        Console.ReadKey();
+    }
+}
+
+
---
+++ RollDie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDRollDice
+{
+    class RollDie
+    {
+        private Random random;
+        private int sidesCount;
+
+        public RollDie(int aSidesCount)
+        {
+            sidesCount = aSidesCount;
+            random = new Random();
+        }
+        public int GetSidesCount()
+        {
+            return sidesCount;
+        }
+        public int Roll()
+        {
+            return random.Next(1, sidesCount + 1);
+        }
+        public override string ToString()
+        {
+            return String.Format("Rolling a die with {0} sides", sidesCount);
+        }
+
+    }
+}
