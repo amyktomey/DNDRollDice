ce00d1c289ecb371adc48fb873a32eae313cf003
Amy Tomey <amy.tomey59@@gmail.com>
2022-03-28 21:16:18 -0400

    commented out code in tests that throw errors, updated readMe

--- Program.cs
+++ Program.cs
@@ -2,69 +2,55 @@
 
 class Program
 {
+    private static object userInput = "Y";
+
     static void Main(string[] args)
     {
-        if (args is null)
+        while (userInput == "Y")
         {
-            throw new ArgumentNullException(nameof(args));
-        }
+            Console.Clear();
+            Console.WriteLine("Welcome! This is a D&D dice rolling app.");
 
-        Console.WriteLine("Welcome! This is a D&D dice rolling app.");
-       
-        Console.WriteLine("Which dice do you need to roll? ");
+            Console.WriteLine("Which dice do you need to roll? ");
 
-        //list available dice
-        foreach(KeyValuePair<string, int> die in DNDDiceTypes.numberOfSides)
-        {
-            Console.WriteLine(die.Key, die.Value);
-        }
-        Console.WriteLine("Please enter the the number of sides.");
-        Console.WriteLine("**For Percantage: input 100**");
+            //list available dice
+            foreach (KeyValuePair<string, int> die in DNDDiceTypes.numberOfSides)
+            {
+                Console.WriteLine(die.Key, die.Value);
+            }
+            Console.WriteLine("Please enter the the number of sides.");
+            Console.WriteLine("**For Percantage: input 100**");
 
-        // retrieve dice type
-        var sidesCountString = Console.ReadLine( );
-        var sidesCount = Int32.Parse(sidesCountString);
+            // retrieve dice type
+            var sidesCountString = Console.ReadLine();
+            var sidesCount = Int32.Parse(sidesCountString);
 
-        //add exception if entry is not in the dictionary or is null
+            //add exception if entry is not in the dictionary or is null
 
 
-        Console.WriteLine("How many times to roll the dice? ");
+            Console.WriteLine("How many times to roll the dice? ");
 
-        var numberOfRolls = Int32.Parse(Console.ReadLine());
-        for (int i = 0; i < numberOfRolls; i++)
-        {
-            var diceRollGenerator = new RollDie(sidesCount);
-            var results = diceRollGenerator.Roll();
-            Console.WriteLine("Here is your roll... " + results);
-        }       
+            var numberOfRolls = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                var diceRollGenerator = new RollDie(sidesCount);
+                var results = diceRollGenerator.Roll();
+                Console.WriteLine("Here is your roll... " + results);
+            }
 
-        //static bool Prompt4YesNo(string prompt)
-        //{
-        //    string userInput = "";
-        //    do
-        //    {
-        //        Console.Write("prompt");
-        //        userInput = Console.ReadLine();
-        //    } while (userInput.ToUpper() != "Y" && userInput.ToUpper() != "N");
-        //    return userInput.ToUpper() == "Y";
-        //}
-
-        //Roll again or exit
-
-        Console.WriteLine("Would you like to roll again?");
-        Console.WriteLine("Enter Y for yes or N for no.");
-        var userInput = Console.ReadLine();
-
-       while (userInput.ToUpper() == "Y")
-        {
-
-        }
-        static void Exit()
-        {
-            Console.WriteLine("are you sure you would like to leave the program?");
-            Console.WriteLine("Press ENTER to confirm!");
-            Console.ReadLine();
-            System.Environment.Exit(1);
+            Console.WriteLine("Would you like to roll again?");
+            Console.WriteLine("Enter Y for yes or N for no.");
+            var userInput = Console.ReadLine();
+            //if (userInput == "Y")
+            //{
+            //    continue;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Thank you for rolling dice with us.");
+            //    Environment.Exit(0);
+            //    Console.WriteLine("Goodbye");
+           // }
         }
         Console.ReadKey();
     }
--- README.md
+++ README.md
@@ -2,18 +2,17 @@
 A console app to roll the various sided die used in D &amp; D gaming... or any role playing game that uses a variety of dice during play.
 
 *You must create at least one class, then create at least one object of that class and populate it with data.You must use or display the data in your application.*
-RollDie is the class containing  the methods used to roll the dice. 
+RollDie is the class containing the methods used to roll the dice. 
 
 *Create and call at least 3 functions or methods, at least one of which must return a value that is used in your application.*
 In Program.cs,  the requested dice (''sidesCount'')is collected from the end user and  retrieved from the dictionary ''DNDDiceTypes"". 
 The user then specifies the number of rolls (''numberOfRolls'').  The "diceRollGenerator" then rolls the die and prints results to the console.
 	
-//Implement a “master loop” console application where the user can repeatedly enter commands/perform actions, including choosing to exit the program
+*Implement a “master loop” console application where the user can repeatedly enter commands/perform actions, including choosing to exit the program*
+Program asks if you need to roll again. If not, the program ends.
 
 *Create a dictionary or list, populate it with several values, retrieve at least one value, and use it in your program.*
 DNDDiceTypes is a dictionaty that defines the type of dice available by the number of sides.
-
-*//Implement a log that records errors, invalid inputs, or other important events and writes them to a text file
 
 *//Create 3 or more unit tests for your application 
 
